3277d920bfaef075330fcbcc73853f7b94c6ba24
marvin255 <postbox433@gmail.com>
2020-12-28 13:38:47 +0100

    Add RandomIntEngine (#8)

---
+++ Tests/RandomEngine/RandomIntEngineTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marvin255\RandomStringGenerator\Tests\RandomEngine;
+
+use Marvin255\RandomStringGenerator\RandomEngine\RandomIntEngine;
+use Marvin255\RandomStringGenerator\Tests\BaseCase;
+
+class RandomIntEngineTest extends BaseCase
+{
+    /**
+     * @test
+     */
+    public function testRand(): void
+    {
+        $from = 12;
+        $to = 4321;
+
+        $generator = new RandomIntEngine();
+
+        $rand = $generator->rand($from, $to);
+
+        $this->assertGreaterThanOrEqual($from, $rand);
+        $this->assertLessThanOrEqual($to, $rand);
+    }
+}
