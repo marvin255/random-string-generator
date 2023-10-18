eee0730c7f8e3949f20a69238418aa661b43fa09
marvin255 <postbox433@gmail.com>
2023-10-18 10:24:44 +0200

    Add RandomizerFastEngine

---
+++ RandomEngine/RandomizerFastEngine.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marvin255\RandomStringGenerator\RandomEngine;
+
+use Random\Engine\Xoshiro256StarStar;
+use Random\Randomizer;
+
+/**
+ * Object that uses Randomizer object with Random\Engine\Xoshiro256StarStar to create random numbers.
+ *
+ * @internal
+ */
+final class RandomizerFastEngine implements RandomEngine
+{
+    private readonly Randomizer $randomizer;
+
+    public function __construct()
+    {
+        $this->randomizer = new Randomizer(new Xoshiro256StarStar());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function rand(int $min = RandomEngine::RAND_MIN, int $max = RandomEngine::RAND_MAX): int
+    {
+        return $this->randomizer->getInt($min, $max);
+    }
+}
